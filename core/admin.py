233db6b03f5826c1e4932f1b59adc537b3f52465
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-25 18:30:18 +0330

    register model user

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
-
+from . import models
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    model = models.User
+    ordering = ('username',)
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', 'is_superuser')
+    list_display = ['username', 'email', 'is_superuser', 'is_staff', 'is_active']
+
+    fieldsets = (
+        ('Authentication', {
+            "fields": (
+                'username', 'email', 'password', 'first_name', 'last_name', 'mobile'
+            ),
+        }),
+        ('group permissions', {
+            'fields': (
+                'user_permissions', 'groups',
+            )
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_staff', 'is_active', 'is_superuser'
+            ),
+        }),
+        ('last login', {
+            'fields': (
+                'last_login',
+                'date_joined',
+            ),
+        }),
+    )
+
+    add_fieldsets = (
+        ('Create User', {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser')
+        }),
+    )
