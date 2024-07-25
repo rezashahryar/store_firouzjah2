371c887cd9554fdeb61305827efc89af5c598fde
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-26 00:32:40 +0330

    install & config drf-yasg package for swagger(documentation the endpoints)

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # third party apps
     'rest_framework',
+    'drf_yasg',
 
     # local apps
     'core.apps.CoreConfig',
--- core/api/v1/views.py
+++ core/api/v1/views.py
@@ -1,6 +1,4 @@
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
 
 from . import serializers
 from core.models import OtpRequest
