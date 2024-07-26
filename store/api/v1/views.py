737bc0d5f057c173ae7fbb7861039f5f96dfeab3
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-26 23:38:31 +0330

    override get_queryset in ProductListViewSet view in store.views for improve & separate queryset in list & retrieve

--- store/api/v1/views.py
+++ store/api/v1/views.py
@@ -12,14 +12,20 @@ from . import serializers
 class ProductListViewSet(mixins.ListModelMixin,
                         mixins.RetrieveModelMixin,
                         GenericViewSet):
-    queryset = models.Product.objects.select_related('base_product') \
-        .select_related('base_product__category').select_related('base_product__sub_category') \
-            .select_related('base_product__product_type').select_related('base_product__brand') \
-                .select_related('size').select_related('color').prefetch_related(Prefetch(
-                    'base_product__properties',
-                    queryset=models.SetProductProperty.objects.select_related('property')
-                )).all()
     lookup_field = 'slug'
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return models.Product.objects.select_related('base_product') \
+                .select_related('base_product__category').select_related('base_product__sub_category').all()
+        elif self.action == 'retrieve':
+            return models.Product.objects.select_related('base_product') \
+                .select_related('base_product__category').select_related('base_product__sub_category') \
+                    .select_related('base_product__product_type').select_related('base_product__brand') \
+                        .select_related('size').select_related('color').prefetch_related(Prefetch(
+                            'base_product__properties',
+                            queryset=models.SetProductProperty.objects.select_related('property')
+                        )).all()
 
     def get_serializer_class(self):
         if self.action == 'list':
