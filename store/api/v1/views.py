6b9363c6d00ae19d68fb91ff19d93fff4ec0a71c
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-26 23:22:55 +0330

    optimization queryset of ProductListViewSet in store.views

--- store/api/v1/views.py
+++ store/api/v1/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 
@@ -11,7 +12,13 @@ from . import serializers
 class ProductListViewSet(mixins.ListModelMixin,
                         mixins.RetrieveModelMixin,
                         GenericViewSet):
-    queryset = models.Product.objects.all()
+    queryset = models.Product.objects.select_related('base_product') \
+        .select_related('base_product__category').select_related('base_product__sub_category') \
+            .select_related('base_product__product_type').select_related('base_product__brand') \
+                .select_related('size').select_related('color').prefetch_related(Prefetch(
+                    'base_product__properties',
+                    queryset=models.SetProductProperty.objects.select_related('property')
+                )).all()
     lookup_field = 'slug'
 
     def get_serializer_class(self):
