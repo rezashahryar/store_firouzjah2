2b3ef6bef446da3e441e2c9561487c03dac72142
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-07-26 12:27:46 +0330

    config models to show in panel admin of django

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+
+@admin.register(models.CategoryProduct)
+class CategoryProductAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.SubCategoryProduct)
+class SubCategoryProduct(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.Brand)
+class BrandAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.BaseProduct)
+class BaseProductAdmin(admin.ModelAdmin):
+    ...
